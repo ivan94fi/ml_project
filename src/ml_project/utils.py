eb64747354e1f42aa7ba70c94a924150e10d6ad1
Ivan Prosperi <ivan94fi@gmail.com>
2020-09-24 09:28:58 +0200

    Add FastDataloader (not used for now)

--- src/ml_project/utils.py
+++ src/ml_project/utils.py
@@ -31,3 +31,34 @@ def should_print(phase, batch_index, config):
         batch_index % config.print_interval == config.print_interval - 1
         or batch_index == config.batch_numbers["train"] - 1
     )
+
+
+class _RepeatSampler:
+    """Sampler that repeats forever."""
+
+    def __init__(self, sampler):
+        self.sampler = sampler
+
+    def __iter__(self):
+        while True:
+            yield from iter(self.sampler)
+
+
+class FastDataLoader(torch.utils.data.DataLoader):
+    """
+    Dataloader with process reuse taken from pytorch issue 15849.
+
+    See https://github.com/pytorch/pytorch/issues/15849 for more information.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        object.__setattr__(self, "batch_sampler", _RepeatSampler(self.batch_sampler))
+        self.iterator = super().__iter__()
+
+    def __len__(self):
+        return len(self.batch_sampler.sampler)
+
+    def __iter__(self):
+        for _ in range(len(self)):
+            yield next(self.iterator)
